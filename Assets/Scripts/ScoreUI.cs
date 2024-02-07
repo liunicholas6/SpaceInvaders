93d6fa6640161b024964384070ea926693754565
Nicholas Liu <liunicholas6@gmail.com>
2024-02-07 02:05:50 -0500

    Updated score/life panels

--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -1,20 +1,24 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 public class ScoreUI : MonoBehaviour
 {
     Global globalObj;
     Text scoreText;
+    Text livesText;
     // Use this for initialization
     void Start()
     {
         GameObject g = GameObject.Find("GlobalObject");
         globalObj = g.GetComponent<Global>();
-        scoreText = gameObject.GetComponent<Text>();
+        scoreText = transform.Find("ScorePanel").GetComponent<Text>();
+        livesText = transform.Find("LivesPanel").GetComponent<Text>();
     }
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = String.Format("Score: {0}, Lives: {1}", globalObj.score.ToString(), globalObj.lives);
+        scoreText.text = $"Score: {globalObj.score.ToString()}";
+        livesText.text = $"Lives: {globalObj.lives.ToString()}";
     }
 }
